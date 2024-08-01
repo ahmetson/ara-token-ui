dd5a93c4f68f08b380cbb4d11e0d59b265baa873
Medet Ahmetson <milayter@gmail.com>
2024-08-01 19:47:10 +0700

    Fix override

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -43,10 +43,10 @@ export function Hero() {
           <div className='mt-10 flex items-center justify-center gap-x-6'>
             <LinkComponent
               href='/tasks'
-              className='rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'>
+              className='relative rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'>
               Browse tasks
             </LinkComponent>
-            <LinkComponent href='/about' className='text-sm font-semibold leading-6 text-gray-900'>
+            <LinkComponent href='/about' className='relative text-sm font-semibold leading-6 text-gray-900'>
               Learn more <span aria-hidden='true'>→</span>
             </LinkComponent>
           </div>
