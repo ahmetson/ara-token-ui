bd94f67d479b9e2845ad0c679051c81f96ec57d1
Medet Ahmetson <milayter@gmail.com>
2024-08-01 19:43:52 +0700

    Fix the header

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -7,9 +7,7 @@ import { LinkComponent } from './LinkComponent'
 export function Hero() {
   return (
     <div className='relative isolate px-6 pt-14 lg:px-8'>
-      <div
-        aria-hidden='true'
-        className='absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80'>
+      <div aria-hidden='true' className='absolute inset-x-0 transform-gpu overflow-hidden blur-3xl '>
         <div
           style={{
             clipPath:
